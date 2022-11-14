3dafeaae8699c0cef36671a83a12bc18130c7452
Karina Chan <karinachan@Karinas-MacBook-Air.local>
2022-11-14 15:26:18 -0800

    karina

--- source/Visualization1.R
+++ source/Visualization1.R
@@ -1,12 +1,12 @@
 Debt_Distr <- read.csv("https://query.data.world/s/a7nl2auw5ij7tqwmqr52ymsqluqnv3")
-View(Debt_Distr)
+#View(Debt_Distr)
 
 Num_Borrow <- c(Debt_Distr$NumberOfBorrowers)
 
 Group <- c("<$5001", "$5000-$10000", "$10000-$25000", "$25000-$50000", "$50000-$75000", 
            "$75000-$100000", "$100000-$150000", "$150000-$200000", "$200000+")
 
-barplot((Num_Borrow),
+Visual1 <- barplot((Num_Borrow),
         names.arg = Group,
         title(main = "Debt Distribution in 2014",
         xlab = "Debt Balance in 2014",
--- source/Visualization3.R
+++ source/Visualization3.R
@@ -1,6 +1,7 @@
 library(tidyverse)
+
 loan_agestate <- read.csv("https://livingatlas-dcdev.opendata.arcgis.com/datasets/UrbanObservatory::student-loan-balances-by-age-by-state/explore?location=36.882087%2C-112.206293%2C4.52&showTable=true")
-View(loan_agestate)
+#View(loan_agestate)
 
 Locations<- c(loan_agestate$Location)
 
@@ -9,8 +10,6 @@ visual3 <- loan_agestate %>%
              y = dollars_LE24,
              colour = Location)) +
   geom_point()+
-  geom_smooth(method = lm,
-              se = F)+
   labs(x = "Number of Borrowers (Thousands)",
        y = "Dollars Owed (Billions)",
        title = "Student Loans Owed By Those Less Than 24 Years Old In Each State")+
@@ -18,5 +17,7 @@ visual3 <- loan_agestate %>%
 
 
 print(visual3)
+
+#geom_smooth(method = lm, se = F)+
 
 
