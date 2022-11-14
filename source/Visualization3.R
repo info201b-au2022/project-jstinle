d2de7dd92839756137e86b203aaf542bbad36235
Michelle Arquiza <marquiza@cs.washington.edu>
2022-11-14 15:23:20 -0800

    temp fix

--- source/Visualization3.R
+++ source/Visualization3.R
@@ -2,7 +2,6 @@ library(tidyverse)
 
 #Importing data into a dataframe.
 loan_agestate <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-jstinle/main/data/Student_Loan_Balances_by_Age_by_State.csv")
-View(loan_agestate)
 
 Locations<- c(loan_agestate$Location)
 
@@ -14,8 +13,6 @@ loan_agestate %>%
              y = dollars_LE24,
              colour = Location))+
   geom_point()+
-  geom_smooth(method = lm,
-              se = F)+
   labs(x = "Number of Borrowers (Thousands)",
        y = "Dollars Owed (Billions)",
        title = "Student Loans Owed By Those Less Than 24 Years Old In Each State")+
