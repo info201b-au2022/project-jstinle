94951df6868e2884689415c22d375c9401e3bdc7
Karina Chan <karinachan@Karinas-MacBook-Air.local>
2022-11-14 20:58:27 -0800

    yay for scatterplot

--- source/Visualization3.R
+++ source/Visualization3.R
@@ -2,20 +2,22 @@ library(tidyverse)
 
 
 #Importing data into a dataframe.
-loan_agestate <- read.csv("../data/Student_Loan_Balances_by_Age_by_State.csv")
+loan_agestate <- read.csv("~/Documents/info201/project-jstinle/data/Student_Loan_Balances_by_Age_by_State.csv")
 
 Locations<- c(loan_agestate$Location)
 
 #Creating a scatterplot, with each color representing a state.
 #Showing how number of borrowers relates to dollars owed.
 
-loan_agestate %>%
+visual3 <- loan_agestate %>%
   ggplot(aes(x = borrowers_LE24,
              y = dollars_LE24,
-             colour = Location))+
+             colour = Location)) +
   geom_point()+
   labs(x = "Number of Borrowers (Thousands)",
        y = "Dollars Owed (Billions)",
        title = "Student Loans Owed By Those Less Than 24 Years Old In Each State")+
+
   theme_minimal()
+
 
