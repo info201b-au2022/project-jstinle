e4bae1f93879b9951fd49c7b6969c0372c7c6a85
Michelle Arquiza <marquiza@cs.washington.edu>
2022-12-05 15:41:38 -0800

    ui layout

--- docs/app_ui.R
+++ docs/app_ui.R
@@ -1,3 +1,39 @@
+intro_panel <- tabPanel(
+  "Introduction",
+  titlePanel("intro title")
+)
+
+chart1_panel <- tabPanel(
+  "chart 1 descriptor",
+  titlePanel("chart 1 title")
+)
+
+chart2_panel <- tabPanel(
+  "chart 2 descriptor",
+  titlePanel("chart 2 title")
+)
+
+chart3_panel <- tabPanel(
+  "chart 3 descriptor",
+  titlePanel("chart 3 title")
+)
+
+summary_panel <- tabPanel(
+  "Summary",
+  titlePanel("summary title")
+)
+
+report_panel <- tabPanel(
+  "Introduction",
+  titlePanel("report title")
+)
+
 ui <- navbarPage(
-  "Student Loan Debt"
+  "Student Loan Debt",
+  intro_panel,
+  chart1_panel,
+  chart2_panel,
+  chart3_panel,
+  summary_panel,
+  report_panel
 )
