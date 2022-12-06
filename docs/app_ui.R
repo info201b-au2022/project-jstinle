8c43df79d5fbec013d14fa2d519099e385ad0c39
Michelle Arquiza <marquiza@cs.washington.edu>
2022-12-05 19:32:00 -0800

    layout w comments! feel free to change yall

--- docs/app_ui.R
+++ docs/app_ui.R
@@ -1,11 +1,36 @@
-intro_panel <- tabPanel(
-  "Introduction",
-  titlePanel("intro title")
+# -----------------------------------------------------------------------------
+# Michelle's Section
+# Summary + Chart1
+# -----------------------------------------------------------------------------
+
+summary_panel <- tabPanel(
+  "Summary",
+  titlePanel("Takeaways: Loans & Debt Per Generation, Types, and Degrees")
+)
+
+chart1_main_content <- mainPanel(
+  p("placeholder for main panel")
+)
+chart1_sidebar_content <- sidebarPanel(
+  p("placeholder for sidebar panel")
 )
 
 chart1_panel <- tabPanel(
   "chart 1 descriptor",
-  titlePanel("chart 1 title")
+  titlePanel("chart 1 title"),
+  sidebarLayout(
+    chart1_sidebar_content,
+    chart1_main_content
+  )
+)
+
+# -----------------------------------------------------------------------------
+# Justin's Section
+# Intro + Chart2
+# -----------------------------------------------------------------------------
+intro_panel <- tabPanel(
+  "Introduction",
+  titlePanel("introduction titlea")
 )
 
 chart2_panel <- tabPanel(
@@ -13,23 +38,27 @@ chart2_panel <- tabPanel(
   titlePanel("chart 2 title")
 )
 
+
+# -----------------------------------------------------------------------------
+# Karina's Section
+# Report + Chart3
+# -----------------------------------------------------------------------------
+report_panel <- tabPanel(
+  "Report",
+  titlePanel("report title")
+)
+
 chart3_panel <- tabPanel(
   "chart 3 descriptor",
   titlePanel("chart 3 title")
 )
 
-summary_panel <- tabPanel(
-  "Summary",
-  titlePanel("summary title")
-)
 
-report_panel <- tabPanel(
-  "Introduction",
-  titlePanel("report title")
-)
-
+# -----------------------------------------------------------------------------
+# Create UI
+# -----------------------------------------------------------------------------
 ui <- navbarPage(
-  "Student Loan Debt",
+  "Student Loan Debt: The Growing Trillion-Dollar Crisis",
   intro_panel,
   chart1_panel,
   chart2_panel,
